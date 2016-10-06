2935f55fbdca2cf168b96b2734a31b81a2bd8c74
NoMoExcuses <ellucky4@gmail.com>
2016-10-06 22:05:47 +0200

    Improved result checking and few other changes

--- script.js
+++ script.js
@@ -1,63 +1,39 @@
 //(function () {	//IIFE
 	"use strict";
-//Tworzenie 9 divow z id 1,2,3...
-function makefields()
-{
-	var fields ="";
-	
-	for (var i=1; i<=9; i++)
-	{
-		fields = fields +'<div id="'+i+'" class="field" onclick="insert('+i+')"><p>'+i+'</p></div>';
-		document.getElementById("fields").innerHTML=fields;
-//nie działo prawidłowo->		document.getElementById(i).addEventListener("click", function(){insert(i);}, false);
-	}
-}
-makefields();
-
-//Wyswietlenie remisu i usuwanie aktywnosci pól
-function draw()
- {
-	for(var i=1; i<=9; i++) 
-	{
-		document.getElementById(i).onclick="";
-		document.getElementById(i).classList.remove('field');
-		document.getElementById(i).classList.add('off');
-	}
-	document.getElementById("result").innerHTML="<img src='img/remis.png'>";
-}
 
 //Wyswietlenie kto wygral i usuwanie aktywnosci pól
 
 function win(who){
-	for(var i=1; i<=9; i++) 
+	for(var i=0; i<=8; i++) 
 	{
 		document.getElementById(i).onclick="";
+		//var rmInsert = "insert("+i+")";
+		//document.getElementById(i).removeEventListener("click", rmInsert);
 		document.getElementById(i).classList.remove('field');
 		document.getElementById(i).classList.add('off');
 	}
-	document.getElementById("result").innerHTML="<img src='img/win"+who+".png'>";
-	
-	
+	document.getElementById("result").innerHTML="<img src='img/"+who+".png'>";
 }
 
 //Sprawdzanie gdzie zaszla wygrana, czyli gdzie divy w lini sa takie same
 
-function ifwin(who){
-		var divs = new Array;
+function ifWin(who){
+		var divs = [];
 
-		for (var i=1; i<=9; i++)
+		for (var i=0; i<=8; i++)
 		{
 			divs[i]=document.getElementById(i).innerHTML;
 		}
-	if(divs[1] === divs[2] && divs[2] === divs[3] ||
-		divs[4] === divs[5] && divs[5] === divs[6] ||
-		divs[7] === divs[8] && divs[8] === divs[9] ||
+	if(divs[0] === divs[1] && divs[1] === divs[2] ||
+		divs[3] === divs[4] && divs[4] === divs[5] ||
+		divs[6] === divs[7] && divs[7] === divs[8] ||
+		divs[0] === divs[3] && divs[3] === divs[6] ||
 		divs[1] === divs[4] && divs[4] === divs[7] ||
 		divs[2] === divs[5] && divs[5] === divs[8] ||
-		divs[3] === divs[6] && divs[6] === divs[9] ||
-		divs[1] === divs[5] && divs[5] === divs[9] ||
-		divs[3] === divs[5] && divs[5] === divs[7]) 
+		divs[0] === divs[4] && divs[4] === divs[8] ||
+		divs[2] === divs[4] && divs[4] === divs[6]) 
 		win(who);
+	else if(round>9) win("draw");
 }
 
 	
@@ -69,47 +45,47 @@ function insert(i)
 {
 
 	//Porownanie zawartosci poczatkowej diva
-	var div = ""+i;
-	var getstate = document.getElementById(div).innerHTML;
+	
+	var getState = document.getElementById(i).innerHTML;
 	var state="<p>"+i+"</p>";
 	
-	
-	if (round%2==0 && getstate == state) {
+	if (round%2==0 && getState == state) {
 		
 		document.getElementById(i).innerHTML="<img src='img/krzyzyk.png'>";
 		click.play();
-		var who = "x";
-		if(round>=3) ifwin(who);
+		
 		round++;
+		console.log(round);
+		if(round>=3 && round<=10) ifWin("x");
+		return;
 		
-		if (round>9){
-			draw ();
-			return 0;
-		}
-		
-		return 0;
 	}
-	
-	if(round%2!=0 && getstate == state) {
+	if(round%2!=0 && getState == state) {
 		
 		document.getElementById(i).innerHTML="<img src='img/kolko.png'>";
 		click.play();
-		var who = "o";
-		if(round>=3) ifwin(who);
+		
 		round++;
+		console.log(round);
+		if(round>=3 && round<=10) ifWin("o");
+		return;
 		
-		if (round>9){
-			draw ();
-			return 0;
-		}
-		
-		
-		return 0;
 	}
 	
 }
-//Replay button
-document.getElementById("replay").addEventListener("click", function(){location.reload();}, false);
+	//Replay button
+	document.getElementById("replay").addEventListener("click", function(){location.reload();});
+
+
+	//Tworzenie 9 divow z id 1,2,3...
+	var fields ="";
+	
+	for (var i=0; i<=8; i++)
+	{
+		fields = fields +'<div id="'+i+'" class="field" onclick="insert('+i+')"><p>'+i+'</p></div>';
+		document.getElementById("fields").innerHTML=fields;
+//nie działo prawidłowo->		document.getElementById(i).addEventListener("click", function(){insert(i);});
+	}
 
 //})();	//IIFE
 
